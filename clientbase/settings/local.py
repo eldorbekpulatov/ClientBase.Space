40125eba5a85fc09424ca8321dd7d6ee6ecc791e
Eldor Bekpulatov <eb654@cornell.edu>
2021-07-25 21:51:31 -0400

    removed some settings with whitenoise static

--- clientbase/settings/local.py
+++ clientbase/settings/local.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # has to do with any files uploaded from a user
 MEDIA_URL = "/media/"
--- clientbase/settings/production.py
+++ clientbase/settings/production.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # has to do with any files uploaded from a user
